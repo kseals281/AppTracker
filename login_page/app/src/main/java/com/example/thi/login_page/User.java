477d92bae0d18f94eb7baeb0a50bc3c6e9c77294
Khari <kseals281@gmail.com>
2016-10-30 02:07:22 -0400

    FIRST WORKING BUILD :D

---
+++ login_page/app/src/main/java/com/example/thi/login_page/User.java
@@ -0,0 +1,13 @@
+package com.example.thi.login_page;
+
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Created by Khari on 10/30/2016.
+ */
+
+public class User {
+    private Set<String> collegeList;
+    HashMap<String, HashMap<String, Boolean>> collegeRequirements;
+}
